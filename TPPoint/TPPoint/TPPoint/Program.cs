78d5ce382d3769c64e29172a891dcf7cb7087ec3
ethantroel <ethan.troel03pgmail.com>
2022-02-15 14:27:06 +0100

    creationclassepoint

---
+++ TPPoint/TPPoint/TPPoint/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TPPoint
+{
+    class Point
+    {
+        private int _X;
+
+
+        private int _Y;
+
+        public int X
+        {
+            get { return _X; }
+        }
+
+
+        public int Y
+        {
+            get { return _Y; }
+        }
+
+
+    }
+}
